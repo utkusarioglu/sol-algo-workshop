59f92ae16c9ed6369e29fa269ff980465dd2aa72
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-12-14 12:42:15 +0000

    Feat: Add more mapping tests
    - Add tests for static and dynamic arrays inside mappings.

--- src/contracts/Mapping.sol
+++ src/contracts/Mapping.sol
@@ -6,6 +6,8 @@ contract Mapping {
   mapping(uint256 => string) public uint256ToStringStore;
   mapping(uint256 => uint8) public uint256ToUint8Store;
   mapping(uint256 => uint64[]) public uint256ToUint64ArrStore;
+  mapping(uint256 => uint128[2]) public uint256ToUint128StaticStore;
+  mapping(uint256 => bool[4]) public uint256ToBoolStaticStore;
 
   constructor() {
     uint256ToStringStore[1] = "13";
@@ -14,6 +16,9 @@ contract Mapping {
     ] = "------------------------------------------------------------------------------------------------------------------------";
     uint256ToUint8Store[1] = 3;
     uint256ToUint64ArrStore[1] = [2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+    uint256ToUint64ArrStore[2] = [21, 32];
+    uint256ToUint128StaticStore[4] = [7, 13];
+    uint256ToBoolStaticStore[17] = [true, true, false, true];
   }
 
   function setItem(uint256 key, string calldata value) external {
--- tests/Mapping.unit.test.ts
+++ tests/Mapping.unit.test.ts
@@ -229,34 +229,37 @@ describe(CONTRACT_NAME, () => {
       });
 
       describe("uint256ToUint64ArrStore", () => {
-        it.only("Reads dynamic array", async () => {
-          const slotSize = 32;
-          const elemSize = 8; // 64 bits
-          const slot = getSlot("uint256ToUint64ArrStore");
-          const key = 1;
-          const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
-          const keyHex = await getStorageAt(instance.address, keyP);
-          const keyArrayified = arrayify(keyHex);
-          const chunkLength = keyArrayified.at(-1);
-          assert(chunkLength);
-          const elemPerSlot = slotSize / elemSize;
-          const chunkSlotCount = Math.ceil(chunkLength / elemPerSlot);
-          const chunkP1 = solidityKeccak256(["uint256"], [keyP]);
-          const chunks = await Promise.all(
-            Array(chunkSlotCount)
-              .fill(null)
-              .map(async (_, i) =>
-                getStorageAt(
-                  instance.address,
-                  BigNumber.from(chunkP1).add(i).toHexString()
+        describe("Reads dynamic array", () => {
+          /**
+           * Reads a dynamic array using a simple reducer that requires two
+           * `reverse` operations to get the data in the right order.
+           */
+          it("Reads dynamic array 1", async () => {
+            const slotSize = 32;
+            const elemSize = 8; // 64 bits
+            const slot = getSlot("uint256ToUint64ArrStore");
+            const key = 1;
+            const expected = [2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+            const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
+            const keyHex = await getStorageAt(instance.address, keyP);
+            const keyArrayified = arrayify(keyHex);
+            const elemCount = keyArrayified.at(-1);
+            assert(elemCount);
+            const elemPerSlot = slotSize / elemSize;
+            const chunksLength = Math.ceil(elemCount / elemPerSlot);
+            const chunkP1 = solidityKeccak256(["uint256"], [keyP]);
+            const chunks = await Promise.all(
+              Array(chunksLength)
+                .fill(null)
+                .map(async (_, i) =>
+                  getStorageAt(
+                    instance.address,
+                    BigNumber.from(chunkP1).add(i).toHexString()
+                  )
                 )
-              )
-          );
+            );
 
-          const elemSizeHex = elemSize * 2;
-          const values = chunks
-            .reverse()
-            .reduce((p, c) => {
+            const flatReducer = (p: number[], c: string) => {
               for (let i = 0; i < elemPerSlot; i++) {
                 const lowerBound = i * elemSizeHex + 2;
                 const upperBound = (i + 1) * elemSizeHex + 2;
@@ -265,12 +268,217 @@ describe(CONTRACT_NAME, () => {
                 p.push(decimalVal);
               }
               return p;
-            }, [] as number[])
-            .reverse()
-            .slice(0, chunkLength);
+            };
 
-          console.log({ chunks, keyHex, chunkLength, values });
-          expect(true).to.eq(false);
+            const elemSizeHex = elemSize * 2;
+            const values = chunks
+              .reverse()
+              .reduce(flatReducer, [] as number[])
+              .reverse()
+              .slice(0, elemCount);
+            expect(values).to.deep.eq(expected);
+          });
+
+          /**
+           * Reads a dynamic array using a reducer that reads the data in
+           * reverse, avoiding the need for using the `reverse` function.
+           * Uses the `slotIndex` variable to track the elements of the final
+           * array, manipulates inside the `for` block to find their location
+           * inside the hex chunk.
+           */
+          it("Reads dynamic array 2", async () => {
+            const slotSize = 32;
+            const elemSize = 8; // 64 bits
+            const slot = getSlot("uint256ToUint64ArrStore");
+            const key = 1;
+            const expected = [2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+            const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
+            const keyHex = await getStorageAt(instance.address, keyP);
+            const keyArrayified = arrayify(keyHex);
+            const elemCount = keyArrayified.at(-1);
+            assert(elemCount);
+            const elemPerSlot = slotSize / elemSize;
+            const chunksLength = Math.ceil(elemCount / elemPerSlot);
+            const chunkP1 = solidityKeccak256(["uint256"], [keyP]);
+            const chunks = await Promise.all(
+              Array(chunksLength)
+                .fill(null)
+                .map(async (_, i) =>
+                  getStorageAt(
+                    instance.address,
+                    BigNumber.from(chunkP1).add(i).toHexString()
+                  )
+                )
+            );
+
+            const reverseReducer = (
+              p: number[],
+              c: string,
+              chunkIndex: number
+            ) => {
+              loop: for (
+                let slotIndex = elemPerSlot - 1;
+                slotIndex >= 0;
+                slotIndex--
+              ) {
+                if (
+                  chunkIndex === chunksLength - 1 &&
+                  slotIndex < emptyBlocks
+                ) {
+                  continue loop;
+                }
+                const lowerBound = slotIndex * elemSizeHex + 2;
+                const upperBound = (slotIndex + 1) * elemSizeHex + 2;
+                const hexVal = "0x" + c.slice(lowerBound, upperBound);
+                const decimalVal = BigNumber.from(hexVal).toNumber();
+                p.push(decimalVal);
+              }
+              return p;
+            };
+
+            const elemSizeHex = elemSize * 2;
+            const emptyBlocks = chunksLength * elemPerSlot - elemCount;
+            const values = chunks.reduce(reverseReducer, [] as number[]);
+            expect(values).to.deep.eq(expected);
+          });
+
+          /**
+           * Reads a dynamic array using a reverse reducer that uses hex string
+           * indexing rather than indexing for the uint64 sections of the slot
+           * that represent each number. Unlike the previous reducer, this one
+           * handles all determination required for locating the numbers in the
+           * hex string inside the `for()` statement.
+           */
+          it("Reads dynamic array 3", async () => {
+            const slotSize = 32;
+            const elemSize = 8; // 64 bits
+            const slot = getSlot("uint256ToUint64ArrStore");
+            const key = 1;
+            const expected = [2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+            const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
+            const keyHex = await getStorageAt(instance.address, keyP);
+            const keyArrayified = arrayify(keyHex);
+            const elemCount = keyArrayified.at(-1);
+            assert(elemCount);
+            const elemPerSlot = slotSize / elemSize;
+            const chunksLength = Math.ceil(elemCount / elemPerSlot);
+            const chunkP1 = solidityKeccak256(["uint256"], [keyP]);
+            const chunks = await Promise.all(
+              Array(chunksLength)
+                .fill(null)
+                .map(async (_, i) =>
+                  getStorageAt(
+                    instance.address,
+                    BigNumber.from(chunkP1).add(i).toHexString()
+                  )
+                )
+            );
+
+            const reverseLoopHeavyReducer = (
+              p: number[],
+              c: string,
+              chunkIndex: number
+            ) => {
+              const padding = "0x";
+              loop: for (
+                let hexIndex = (elemPerSlot - 1) * elemSizeHex + padding.length;
+                hexIndex >= padding.length;
+                hexIndex = hexIndex - elemSizeHex
+              ) {
+                if (
+                  chunkIndex === chunksLength - 1 &&
+                  hexIndex < emptyBlocks * elemSizeHex + padding.length
+                ) {
+                  continue loop;
+                }
+                const hexVal =
+                  padding + c.slice(hexIndex, hexIndex + elemSizeHex);
+                const decimalVal = BigNumber.from(hexVal).toNumber();
+                p.push(decimalVal);
+              }
+              return p;
+            };
+
+            const elemSizeHex = elemSize * 2;
+            const emptyBlocks = chunksLength * elemPerSlot - elemCount;
+            const values = chunks.reduce(
+              reverseLoopHeavyReducer,
+              [] as number[]
+            );
+
+            expect(values).to.deep.eq(expected);
+          });
+        });
+
+        it("Reads short uint64 array", async () => {
+          const slotSize = 32;
+          const elemByteSize = 8; // 64 bits
+          const elemHexSize = elemByteSize * 2;
+          const slotMaxElemCount = slotSize / elemByteSize;
+          assert(Number.isInteger(slotMaxElemCount));
+          const slot = getSlot("uint256ToUint64ArrStore");
+          const key = 2;
+          const expected = [21, 32];
+          const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
+          const keyHex = await getStorageAt(instance.address, keyP);
+          const elemCount = arrayify(keyHex).at(-1);
+          assert(elemCount);
+          const blobP = solidityKeccak256(["uint256"], [keyP]);
+          const blobHex = await getStorageAt(instance.address, blobP);
+          const padding = "0x";
+          const members = Array(elemCount)
+            .fill(null)
+            .map((_, i) => {
+              const start =
+                padding.length + (slotMaxElemCount - i - 1) * elemHexSize;
+              const end = padding.length + (slotMaxElemCount - i) * elemHexSize;
+              return BigNumber.from(
+                padding + blobHex.slice(start, end)
+              ).toNumber();
+            });
+          expect(members).to.deep.eq(expected);
+        });
+      });
+
+      describe("uint256ToUint128StaticStore", () => {
+        it("Reads static uint128 array in mapping", async () => {
+          const slotByteSize = 32;
+          const elemByteSize = 16;
+          const elemHexSize = elemByteSize * 2;
+          const elemPerSlot = slotByteSize / elemByteSize;
+          assert(Number.isInteger(elemPerSlot));
+          const slot = getSlot("uint256ToUint128StaticStore");
+          const key = 4;
+          const expected = [7, 13];
+          const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
+          const keyHex = await getStorageAt(instance.address, keyP);
+          const hexIndicator = "0x";
+          const members = Array(expected.length)
+            .fill(null)
+            .map((_, i) => {
+              const start =
+                hexIndicator.length + (elemPerSlot - i - 1) * elemHexSize;
+              const end = hexIndicator.length + (elemPerSlot - i) * elemHexSize;
+              const hexValue = hexIndicator + keyHex.slice(start, end);
+              return BigNumber.from(hexValue).toNumber();
+            });
+          expect(members).to.eq(expected);
+        });
+      });
+
+      describe("uint256ToBoolStaticStore", () => {
+        it.only("Reads bool static bool values in mapping", async () => {
+          const slotByteSize = 32;
+          const slot = getSlot("uint256ToBoolStaticStore");
+          const key = 17;
+          const expected = [true, true, false, true];
+          const keyP = solidityKeccak256(["uint256", "uint256"], [key, slot]);
+          const keyHex = await getStorageAt(instance.address, keyP);
+          const keyArrayified = arrayify(keyHex);
+          const members = keyArrayified.slice(-expected.length).reverse();
+          assert(members.length == 4);
+          const response = Array.from(members).map((n) => !!n);
+          expect(response).to.deep.eq(expected);
         });
       });
     });

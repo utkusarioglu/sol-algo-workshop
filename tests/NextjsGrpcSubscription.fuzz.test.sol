014e75f7a9e8f265aca1f35962c2f974bc14d65f
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-05-30 16:07:54 +0000

    experiment/chore/ethers-v6-integration/3

--- tests/NextjsGrpcSubscription.fuzz.test.sol
+++ tests/NextjsGrpcSubscription.fuzz.test.sol
@@ -4,17 +4,7 @@ pragma solidity 0.8.16;
 import "../src/contracts/NextjsGrpcSubscription.sol";
 
 contract NextjsGrpcSubscriptionFuzz is NextjsGrpcSubscription {
-  // uint256 public subscriptionSeconds;
-
   event AssertionFailed();
-
-  // function echidna_epochCostIsStable() public pure {
-  //   assert(epochCost == 2);
-  // }
-
-  // function echidna_balancesAlwaysPositive() public view returns (bool) {
-  //   return balances[msg.sender].balance >= 0;
-  // }
 
   function aaaa(uint256 subscriptionSeconds) public {
     subscribe(subscriptionSeconds);
